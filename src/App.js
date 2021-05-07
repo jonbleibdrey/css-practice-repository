645b8f13bac0d5168979df754549e3f90aa9ba20
jonathan bleibdrey <jonbleibdrey@aol.com>
2021-05-07 11:20:54 -0400

    inital push and figured out flexbox

--- src/App.js
+++ src/App.js
@@ -61,6 +61,35 @@ function App() {
         </p>
       </div>
     </div>
+    <div className="app__container3">
+      <div className="container3__box">
+        <h3> box four</h3>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Obcaecati
+          repellendus sapiente aperiam veritatis eveniet totam quo officiis ex
+          laudantium deserunt unde voluptas consectetur iste, pariatur
+          voluptate. Numquam voluptatem accusantium consequatur.
+        </p>
+      </div>
+      <div className="container3__box">
+        <h3> box five</h3>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Obcaecati
+          repellendus sapiente aperiam veritatis eveniet totam quo officiis ex
+          laudantium deserunt unde voluptas consectetur iste, pariatur
+          voluptate. Numquam voluptatem accusantium consequatur.
+        </p>
+      </div>
+      <div className="container3__box">
+        <h3> box six</h3>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Obcaecati
+          repellendus sapiente aperiam veritatis eveniet totam quo officiis ex
+          laudantium deserunt unde voluptas consectetur iste, pariatur
+          voluptate. Numquam voluptatem accusantium consequatur.
+        </p>
+      </div>
+    </div>
     </>
   );
 }
